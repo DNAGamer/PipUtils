afa36602c858758d5a23d130370ff8c61e864900
DNA <DNAGamer@users.noreply.github.com>
2017-05-17 15:14:22 +0100

    Update piputils.py

--- piputils.py
+++ piputils.py
@@ -1,6 +1,29 @@
 import os
 import sys
 
+def installPip():
+    import requests
+    print("Downloading pip...")
+    requests.get("https://bootstrap.pypa.io/get-pip.py")
+    os.system("cls")
+    print("Running installer...")
+    paths = sys.path
+    if os.system("{}\python3.5 get-pip.py") != 0:
+        print("Error")
+        exit()
+    print("Adding pip to environment variables...")
+    paths = sys.path
+    direct = paths[4]
+    direct += "\scripts"
+    if os.system('setx PATH "%PATH%;{}\Scripts'.format(direct)) != 0:
+        print("Error adding")
+        exit()
+    print("Completed installation...")
+    print("Cleaning up ^-^")
+    os.unlink("get-pip.py")
+    print("Terminating instance")
+    exit()
+
 def getdir():
     paths = sys.path
     direct = paths[4]
@@ -27,21 +50,21 @@ def update(direct):
     
 direct = getdir()
 while True:
-    mode = input("Install, update, or remove? ")
+    mode = str(input("1. Install a module\n2. Remove a module\n3. Update modules\n4. Install pip properly"))
     mode = mode.lower()
     
-    if mode == "install" or mode == "remove":
+    if mode == "1" or mode == "2":
         module = input("Whats the module called? ")
-        if mode == "install":
+        if mode == "1":
             install(module, direct)
-        elif mode == "remove":
+        elif mode == "2":
             remove(module, direct)
         else:
             print("something went wrong")
-            
-    elif mode == "update":
+    elif mode == "3":
         update(direct)
-        
+    elif mode == "4":
+        installPip()
     else:
         print("That isnt a known mode, try again")
     
